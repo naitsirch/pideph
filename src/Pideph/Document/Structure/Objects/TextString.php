2016ba33874f9ef04db5e496fc63ee7383b71202
naitsirch <naitsirch@e.mail.de>
2016-10-21 22:53:49 +0200

    Added TextString class

---
+++ src/Pideph/Document/Structure/Objects/TextString.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Pideph\Document\Structure\Objects;
+
+/**
+ * Pideph\Document\Structure\Objects\TextString
+ *
+ * See Adobe PDF Reference, Edition 2008-7-1
+ *   §7.3.4 String Objects
+ *   §7.9.2 String Object Types
+ *
+ * @author naitsirch
+ */
+class TextString
+{
+
+}
